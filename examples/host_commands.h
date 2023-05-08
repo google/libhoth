e309219f2c9a2d9fc8469dd91c8c7b4bcbae9db5
Pai Peng <paipeng@google.com>
2023-05-08 13:32:30 -0700

    Add the 'statistics' command

--- examples/host_commands.h
+++ examples/host_commands.h
@@ -156,6 +156,114 @@ struct ec_response_flash_spi_info {
 // NOTE: All further commands in this file are offset by
 // EC_CMD_BOARD_SPECIFIC_BASE.
 
+struct boot_timing_data {
+  uint32_t start_us;
+  uint32_t end_us;
+};
+
+/* Get various statistics */
+#define EC_PRV_CMD_HOTH_GET_STATISTICS 0x000F
+struct ec_response_statistics {
+  /*
+   * Number of 32 bit words returned from this command.
+   * That's not including the reserved fields.
+   *  Offset: 0
+   */
+  uint32_t valid_words;
+
+  /*
+   * The set of flags which describe the most recent reset.  See
+   * include/system.h RESET_FLAG_* for details.
+   *  Offset: 1 (32 bit words)
+   */
+  uint32_t hoth_reset_flags;
+
+  /*
+   * Number of microseconds since the last hoth boot.
+   *  Offset: 2
+   */
+  uint64_t time_since_hoth_boot_us;
+
+  /*
+   * The current temperature of the hoth chip. This is just the value
+   * in the SUM8 register, no conversion to celsius or fahrenheit is applied.
+   * The value returned is a 9.3 bit fixed point binary number. Anything
+   * greater than the max value of a 9.3 bit fixed point binary number is
+   * considered invalid. Default invalid return value is 0xFFFFFFFF.
+   *  Offset: 4
+   */
+  uint32_t hoth_temperature;
+
+  /*
+   * The current INFO strike count in the RO region.
+   * Offset: 5
+   */
+  uint32_t ro_info_strikes;
+
+  /*
+   * The current INFO strike count in the RW region.
+   * Offset: 6
+   */
+  uint32_t rw_info_strikes;
+
+  /*
+   * For testing, a scratch value to say something
+   * Debug only, should be zero in release builds
+   * Offset: 7
+   */
+  uint32_t scratch_value;
+
+  /*
+   * Reason code for last payload update failure.
+   */
+  uint16_t payload_update_failure_reason;
+
+  /*
+   * Reason for last firmware update failure.
+   */
+  uint16_t firmware_update_failure_reason;
+
+  /*
+   * Minor version of the last firmware update that failed.
+   */
+  uint32_t failed_firmware_minor_version;
+
+  /*
+   * Time in microseconds of various things we want to measure during
+   * bootup.  All times are in microseconds.
+   * total - Time from reset to boot up
+   * update - Time spent in the self update routine.  Since a proper self update
+   *          involves a reset, this time is always expected to be low.
+   * mirroring - Time spent mirroing the self-update.  This time is a reasonable
+   *             proxy for the total self update time.
+   * payload_validation - Time spent validating the payload, copying mutable
+   *                      regions and/or dealing with failsafe fallback.
+   */
+  struct boot_timing_data boot_timing_total;
+  struct boot_timing_data boot_timing_firmware_update;
+  struct boot_timing_data boot_timing_firmware_mirroring;
+  struct boot_timing_data boot_timing_payload_validation;
+
+  /*
+   * Confirmation cookie for Payload Update
+   */
+  uint32_t payload_update_confirmation_cookie_failure_reason;
+  uint64_t payload_update_confirmation_cookie;
+
+  /*
+   * Error code returned by a bootloader update failure.
+   */
+  uint32_t bootloader_update_error;
+
+  /*
+   * Future expansion.
+   */
+  uint32_t reserved[42];
+} __attribute__((packed));
+
+#define STATISTIC_OFFSET(field) \
+  (offsetof(struct ec_response_statistics, field) / sizeof(uint32_t))
+
 #define EC_PRV_CMD_HOTH_CHIP_INFO 0x0010
 struct ec_response_chip_info {
   uint64_t hardware_identity;
