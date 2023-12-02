44800076301693bb875c3922636ceeed0f462fb1
Dawid Dabrowski <dawiddabrowski@google.com>
2023-12-01 18:59:55 -0800

    Increase libusb timeout to support payload update.
    The CONTINUE step was failing with 5s timeout during manual testing.
    Increasing the timeout to 15s fixes the problem and should not cause
    additional issue.

--- examples/htool.c
+++ examples/htool.c
@@ -594,7 +594,7 @@ int htool_exec_hostcmd(struct libhoth_device* dev, uint16_t command,
   } resp;
   size_t resp_size;
   status = libhoth_receive_response(dev, &resp, sizeof(resp), &resp_size,
-                                    /*timeout_ms=*/5000);
+                                    /*timeout_ms=*/15000);
   if (status != LIBHOTH_OK) {
     fprintf(stderr, "libhoth_usb_receive_response() failed: %d\n", status);
     return -1;
