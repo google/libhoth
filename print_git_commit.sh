6bc24a1de6ed1cc94a8c4b1b90c5de103e5f40c4
nicholasnooney <nicholasnooney@google.com>
2025-05-27 10:41:35 -0400

    Add support for obtaining git version when using jj
    The version strings are now prefixed with either "jj-" or "git-",
    since different versions use different ids.

--- print_git_commit.sh
+++ print_git_commit.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
 # https://bazel.build/docs/user-manual#workspace-status
+#
+# Custom script to generate the STABLE_GIT_COMMIT variable
+# used by other tooling.
 
-if ! commit=$(git describe --always --dirty --abbrev=40); then
-  >&2 echo "failed to get SHA-1 of HEAD"
-  exit 1
+# Try git first
+commit=$(git describe --always --dirty --abbrev=40 2>/dev/null)
+if [ $? -eq 0 ]; then
+  echo "STABLE_GIT_COMMIT git-$commit"
+  exit 0
 fi
 
-echo "STABLE_GIT_COMMIT $commit"
+# git failed, try jj (https://jj-vcs.github.io/)
+changeid=$(jj show -T change_id --no-patch 2>/dev/null)
+if [ $? -eq 0 ]; then
+  echo "STABLE_GIT_COMMIT jj-$changeid"
+  exit 0
+fi
 
+# None worked
+>&2 echo "failed to get commit/change revision"
+exit 1
--- print_version_header.sh
+++ print_version_header.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# https://bazel.build/docs/user-manual#workspace-status
+#
+# Custom script to obtain the STABLE_GIT_COMMIT variable
+# from bazel and generate a header file that defines the
+# value.
+
 git_commit=$(sed -nE 's/^STABLE_GIT_COMMIT ([a-z0-9\-]+)$/\1/p' bazel-out/stable-status.txt)
 
 if [ -z "$git_commit" ]; then
