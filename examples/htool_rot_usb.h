52d058f31e2a3abb9dd6744f4866f7ab26e95af1
lpsingh <lpsingh@google.com>
2025-06-23 17:37:01 -0700

    Fix terminology for I2C mux control and USB mux control commands
    Replaces the term "target" with "rot" at appropriate places, and deprecate the old commands that use "target"
    
    Hide deprecated commands when listing commands. Also, print a warning when deprecated commands are used
    
    PiperOrigin-RevId: 774990667

--- examples/htool_rot_usb.h
+++ examples/htool_rot_usb.h
@@ -12,33 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef LIBHOTH_EXAMPLES_HTOOL_TARGET_USB_H_
-#define LIBHOTH_EXAMPLES_HTOOL_TARGET_USB_H_
+#ifndef LIBHOTH_EXAMPLES_HTOOL_ROT_USB_H_
+#define LIBHOTH_EXAMPLES_HTOOL_ROT_USB_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define TARGET_USB_MUXCTRL_CMD_STR "mux_ctrl"
-#define TARGET_USB_MUXCTRL_GET_SUBCMD_STR "get"
-#define TARGET_USB_MUXCTRL_CONNECT_TARGET_TO_HOST_SUBCMD_STR \
-  "connect_target_to_host"
-#define TARGET_USB_MUXCTRL_CONNECT_TARGET_TO_FRONT_PANEL \
-  "connect_target_to_front_panel"
+#define ROT_USB_MUXCTRL_CMD_STR "mux_ctrl"
+#define ROT_USB_MUXCTRL_GET_SUBCMD_STR "get"
+#define ROT_USB_MUXCTRL_CONNECT_TARGET_TO_HOST_SUBCMD_STR \
+  "connect_target_to_host"  // Deprecated
+#define ROT_USB_MUXCTRL_CONNECT_TARGET_TO_FRONT_PANEL \
+  "connect_target_to_front_panel"  // Deprecated
+#define ROT_USB_MUXCTRL_CONNECT_ROT_TO_HOST_SUBCMD_STR "connect_rot_to_host"
+#define ROT_USB_MUXCTRL_CONNECT_ROT_TO_FRONT_PANEL "connect_rot_to_front_panel"
 
 // Forward declaration
 struct htool_invocation;
 
-int htool_target_usb_muxctrl_get(const struct htool_invocation* inv);
+int htool_rot_usb_muxctrl_get(const struct htool_invocation* inv);
 
-int htool_target_usb_muxctrl_connect_target_to_host(
+int htool_rot_usb_muxctrl_connect_rot_to_host(
     const struct htool_invocation* inv);
 
-int htool_target_usb_muxctrl_connect_target_to_front_panel(
+int htool_rot_usb_muxctrl_connect_rot_to_front_panel(
     const struct htool_invocation* inv);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif  // LIBHOTH_EXAMPLES_HTOOL_TARGET_USB_H_
+#endif  // LIBHOTH_EXAMPLES_HTOOL_ROT_USB_H_
