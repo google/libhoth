bd42eb3a5e87376c9e7b3576455032dbea3f87fe
Ellis Sarza-Nguyen <sarzanguyen@google.com>
2025-10-22 13:46:44 -0700

    [protocol] Add remaining unit test for auth record
    We are seeking to fill in missing code coverage for the auth record for libhoth.
    This will coverage some negative test cases that we should expect to see fail give
    known specific values. This includes that failing case of a build record. In addition,
    to a mismatch key id with the auth record
    
    Signed-off-by: Ellis Sarza-Nguyen <sarzanguyen@google.com>

--- protocol/authz_record_test.cc
+++ protocol/authz_record_test.cc
@@ -114,6 +114,77 @@ TEST_F(LibHothTest, authz_build_test) {
   EXPECT_EQ(record.key_id, 1);
 }
 
+TEST_F(LibHothTest, authz_build_fail_test) {
+  struct hoth_response_chip_info chipinfo = {};
+  chipinfo.hardware_identity = 0xABCD;
+  chipinfo.hardware_identity |= (0x1234UL << 32);
+
+  // key_id supported by RO and RW. These key_id's are expected to match one
+  // another to successfully program an authorization record. key_id == 0 should
+  // be interpreted as an unknown key_id. Here we set both to 0 to trigger failure.
+  struct hoth_authz_record_get_nonce_response nonce_resp = {};
+  nonce_resp.ro_supported_key_id = 0;
+  nonce_resp.rw_supported_key_id = 0;
+
+  EXPECT_CALL(mock_, send(_,
+                          UsesCommand(HOTH_CMD_BOARD_SPECIFIC_BASE +
+                                      HOTH_PRV_CMD_HOTH_CHIP_INFO),
+                          _))
+      .WillOnce(Return(LIBHOTH_OK));
+
+  EXPECT_CALL(mock_, send(_,
+                          UsesCommand(HOTH_CMD_BOARD_SPECIFIC_BASE +
+                                      HOTH_PRV_CMD_HOTH_GET_AUTHZ_RECORD_NONCE),
+                          _));
+
+  EXPECT_CALL(mock_, receive)
+      .WillOnce(
+          DoAll(CopyResp(&chipinfo, sizeof(chipinfo)), Return(LIBHOTH_OK)))
+      .WillOnce(
+          DoAll(CopyResp(&nonce_resp, sizeof(nonce_resp)), Return(LIBHOTH_OK)));
+
+  uint32_t cap = 123;
+  struct authorization_record record = {};
+
+  EXPECT_EQ(libhoth_authz_record_build(&hoth_dev_, cap, &record), -1);
+}
+
+TEST_F(LibHothTest, authz_mismatch_key_id_test) {
+  struct hoth_response_chip_info chipinfo = {};
+  chipinfo.hardware_identity = 0xABCD;
+  chipinfo.hardware_identity |= (0x1234UL << 32);
+
+  // key_id supported by RO and RW. These key_id's are expected to match one
+  // another to successfully program an authorization record. key_id == 0 should
+  // be interpreted as an unknown key_id. Here we set both to different values to
+  // trigger failure.
+  struct hoth_authz_record_get_nonce_response nonce_resp = {};
+  nonce_resp.ro_supported_key_id = 0;
+  nonce_resp.rw_supported_key_id = 1;
+
+  EXPECT_CALL(mock_, send(_,
+                          UsesCommand(HOTH_CMD_BOARD_SPECIFIC_BASE +
+                                      HOTH_PRV_CMD_HOTH_CHIP_INFO),
+                          _))
+      .WillOnce(Return(LIBHOTH_OK));
+
+  EXPECT_CALL(mock_, send(_,
+                          UsesCommand(HOTH_CMD_BOARD_SPECIFIC_BASE +
+                                      HOTH_PRV_CMD_HOTH_GET_AUTHZ_RECORD_NONCE),
+                          _));
+
+  EXPECT_CALL(mock_, receive)
+      .WillOnce(
+          DoAll(CopyResp(&chipinfo, sizeof(chipinfo)), Return(LIBHOTH_OK)))
+      .WillOnce(
+          DoAll(CopyResp(&nonce_resp, sizeof(nonce_resp)), Return(LIBHOTH_OK)));
+
+  uint32_t cap = 123;
+  struct authorization_record record = {};
+
+  EXPECT_EQ(libhoth_authz_record_build(&hoth_dev_, cap, &record), -1);
+}
+
 TEST_F(LibHothTest, authz_nonce_fail_test) {
   struct hoth_response_chip_info chipinfo = {};
   chipinfo.hardware_identity = 0xABCD;
@@ -160,3 +231,4 @@ TEST_F(LibHothTest, authz_set_test) {
   struct authorization_record record = {};
   EXPECT_EQ(libhoth_authz_record_set(&hoth_dev_, &record), LIBHOTH_OK);
 }
+
