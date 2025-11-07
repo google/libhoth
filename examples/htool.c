b8e4a7834b2f00853422812642c598ff2bf6198a
Kor Nielsen <kor@google.com>
2025-11-06 16:52:45 -0800

    Increase the SPI transport timeout to a reasonable value. (#159)

--- examples/htool.c
+++ examples/htool.c
@@ -1566,7 +1566,7 @@ static const struct htool_param GLOBAL_FLAGS[] = {
      .desc = "Clock speed (in Hz) to use when using spidev transport. Default "
              "behavior (with input 0) is to not change the clock speed"},
     {HTOOL_FLAG_VALUE, .name = "spidev_device_busy_wait_timeout",
-     .default_value = "1000",
+     .default_value = "180000000",
      .desc = "Maximum duration (in microseconds) to wait when SPI device "
              "indicates that it is busy"},
     {HTOOL_FLAG_VALUE, .name = "spidev_device_busy_wait_check_interval",
