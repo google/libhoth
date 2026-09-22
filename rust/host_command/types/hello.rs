caf0403ba137b2aca7d10a1ea08369368efe109b
Sean Portley (Agent) <corpagent-eng-sportley@google.com>
2026-09-22 22:52:11 +0000

    Move host_command hello types into rust/host_command/types:hello

---
+++ rust/host_command/types/hello.rs
@@ -0,0 +1,38 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![no_std]
+
+use pie_rot_hostcmd_types::HostCmdReq;
+use pie_rot_hostcmd_types::HostCmdResp;
+use pie_rot_hostcmd_types::HostCommand;
+use zerocopy::{FromBytes, Immutable, IntoBytes, KnownLayout};
+
+#[derive(FromBytes, Immutable, KnownLayout, IntoBytes, Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+pub struct HelloRequestData {
+    pub challenge: u32,
+}
+impl HostCmdReq for HelloRequestData {
+    const COMMAND: HostCommand = HostCommand::HELLO;
+    const VERSION: u8 = 0;
+    type Response = HelloResponseData;
+}
+
+#[derive(FromBytes, Immutable, KnownLayout, IntoBytes, Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+pub struct HelloResponseData {
+    pub response: u32,
+}
+impl HostCmdResp for HelloResponseData {}
